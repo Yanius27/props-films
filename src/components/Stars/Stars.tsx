fb41761eaa87a3aec6b3d17545e816ff443761be
Yanius27 <zhzh-1987@mail.ru>
2024-11-01 09:21:35 +0700

    Rate component done

---
+++ src/components/Stars/Stars.tsx
@@ -0,0 +1,21 @@
+import React from 'react';
+
+import Star from '../Star';
+
+import './Stars.css';
+
+interface IStarsProps {
+  count: number,
+}
+
+export default function Stars(props: IStarsProps) {
+  const {count} = props;
+
+  if (count < 1 || count > 5 || typeof count !== 'number') return;
+
+  return (
+    <ul className="card-body-stars u-clearfix">
+      {[...Array(count)].map((index) => <li><Star key={index}/></li>)}
+    </ul>
+  );
+}
